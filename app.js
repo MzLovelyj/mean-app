de82b4a359f8f4dd9885cd02edac58b23e595206
mzlovelyj <liljp064@gmail.com>
2017-08-13 14:36:59 -0400

    1st commit

---
+++ app.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+//mongoose configuration
+//HARDCODED ----------| |----------------
+// mongoose.connect('mongodb://localhost/angular2-meanApp');
+mongoose.connect(process.env.MONGODB_URI);
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ 'extended': 'false' }));
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// OUR ROUTES HERE
+// ----------------------------------------------------------
+const book = require('./routes/book');
+app.use('/book', book);
+
+// ----------------------------------------------------------
+
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handler
+app.use((err, req, res, next) => {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+module.exports = app;
+module.exports = app;
