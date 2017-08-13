3bd708b9ec37b5b1593377a7a66a904f58de3e34
mzlovelyj <liljp064@gmail.com>
2017-08-13 18:25:56 -0400

    need to fix redirect afteradding

--- models/Book.js
+++ models/Book.js
@@ -1,11 +1,10 @@
-const mongoose = require('mongoose');
+var mongoose = require('mongoose');
 
 var BookSchema = new mongoose.Schema({
     isbn: String,
     title: String,
     author: String,
     publisher: String,
-    published_date: Date,
     price: Number,
     updated_at: { type: Date, default: Date.now },
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,23 +6,33 @@ import { RouterModule, Routes } from '@angular/router';
 import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 import { AppComponent } from './app.component';
-import { BookService } from './book.service';
 import { BookComponent } from './book/book.component';
+import { BookService } from './book.service';
+import { BookDetailComponent } from './book-detail/book-detail.component';
+import { BookCreateComponent } from './book-create/book-create.component';
+import { BookEditComponent } from './book-edit/book-edit.component';
 
-const ROUTES = [
+const appRoutes: Routes = [
   { path: '', redirectTo: 'books', pathMatch: 'full' },
-  { path: 'books', component: BookComponent }
+  { path: 'books', component: BookComponent },
+  { path: 'book-details/:id', component: BookDetailComponent },
+  { path: 'book-create', component: BookCreateComponent },
+  { path: 'book-edit/:id', component: BookEditComponent }
 ];
+
 @NgModule({
   declarations: [
     AppComponent,
     BookComponent,
+    BookDetailComponent,
+    BookCreateComponent,
+    BookEditComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    RouterModule.forRoot(ROUTES)
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [
     BookService,
